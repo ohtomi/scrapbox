4375cd42f43d45be401f24c51bcd550e349065e0
ohtomi <ohtomi.kenichi@gmail.com>
2017-01-30 01:08:06 +0900

    Move test helper functions to list sub command test

--- command/open_test.go
+++ command/open_test.go
@@ -2,66 +2,11 @@ package command
 
 import (
 	"bytes"
-	"fmt"
-	"io"
-	"net/http"
-	"net/http/httptest"
-	"os"
-	"path"
 	"strings"
 	"testing"
 
-	"github.com/mitchellh/cli"
+	_ "github.com/mitchellh/cli"
 )
-
-func SetTestEnv(key, newValue string) func() {
-
-	prevValue := os.Getenv(key)
-	os.Setenv(key, newValue)
-	reverter := func() {
-		os.Setenv(key, prevValue)
-	}
-	return reverter
-}
-
-func NewTestMeta(outStream, errStream io.Writer, inStream io.Reader) *Meta {
-
-	return &Meta{
-		Ui: &cli.BasicUi{
-			Writer:      outStream,
-			ErrorWriter: errStream,
-			Reader:      inStream,
-		}}
-}
-
-func RunAPIServer() *httptest.Server {
-
-	muxAPI := http.NewServeMux()
-	testAPIServer := httptest.NewServer(muxAPI)
-
-	muxAPI.HandleFunc("/api/pages/go-scrapbox/search/query", func(w http.ResponseWriter, r *http.Request) {
-		query := r.URL.Query()
-		skip := query.Get("skip")
-		limit := query.Get("limit")
-		tags := query["q"]
-
-		filename := fmt.Sprintf("%s-%s", skip, limit)
-		directory := path.Join("../testdata/query/scrapbox.io/go-scrapbox", path.Join(tags...))
-		filepath := path.Join(directory, EncodeFilename(filename))
-		http.ServeFile(w, r, filepath)
-	})
-
-	muxAPI.HandleFunc("/api/pages/go-scrapbox/", func(w http.ResponseWriter, r *http.Request) {
-		urlPath := r.URL.Path
-
-		filename := strings.Replace(urlPath, "/api/pages/go-scrapbox/", "", -1)
-		directory := "../testdata/page/scrapbox.io/go-scrapbox"
-		filepath := path.Join(directory, EncodeFilename(filename))
-		http.ServeFile(w, r, filepath)
-	})
-
-	return testAPIServer
-}
 
 func TestOpenCommand__print_url_having_paren(t *testing.T) {
 
